ba3b86ed11fb33c48218495148bfb42965471dc7
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2021-02-08 23:48:09 -0600

    update app.js

--- react-simple-blog/src/App.js
+++ react-simple-blog/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import CreateNewPost from './Components/CreateNewPost';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <CreateNewPost />
     </div>
   );
 }
